31303e19f12ee4d7665995f686f3209a00bf79d9
Rattuti <i.ishiharar.96296@gmail.com>
2022-07-04 15:25:37 +0900

    sample

--- app/jobs/async_log_job.rb
+++ app/jobs/async_log_job.rb
@@ -1,5 +1,5 @@
 class AsyncLogJob < ApplicationJob
-  queue_as :default
+  queue_as :async_log
 
   def perform(message: "hello") # message引数を追加
     AsyncLog.create!(message: message) # データベースに保存する
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  if Rails.env.development?
+    require "sidekiq/web"
+    mount Sidekiq::Web => "/sidekiq"
+  end
 end
